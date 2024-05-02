978f9302f8385323f25f2533629a8c17f4b960d6
RemainderYears0226 <144605230+RemainderYears0226@users.noreply.github.com>
2024-05-02 10:07:25 +0800

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,20 +1,17 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)
 
-client = MongoClient('mongodb+srv://jimmyjimmy0226:XCvJqawUzPr2aaCd@ctf.bkczoed.mongodb.net/')
-db = client['ctf']
-articles_collection = db['ctf']
+mongo_uri = "mongodb+srv://jimmyjimmy0226:XCvJqawUzPr2aaCd@ctf.bkczoed.mongodb.net/ctf"
+client = MongoClient(mongo_uri)
+db = client.get_database('ctf')
+collection = db.get_collection('ctf')
 
-@app.route('/api/search', methods=['GET'])
-def search():
-    query = request.args.get('query', '')
-    results = articles_collection.find({'$text': {'$search': query}})
-    articles = [{'title': article.get('title', ''), 'content': article.get('content', '')} for article in results]
-    return jsonify(articles)
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = list(collection.find({}, {'_id': 0, 'user_id': 1}))
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True)
